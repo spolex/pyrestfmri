bab79223c8ab078136a61f2400180616ba856838
spolex <spolex@msn.com>
2019-06-02 02:27:38 -0400

    estructurando los directorios de los controles nuevos

--- scripts/controls2bids.sh
+++ scripts/controls2bids.sh
@@ -11,7 +11,11 @@ array=( one two three )
 for i in "${SUBJS[@]}"
 do
 	echo $i
-	mv $INPUT_PATH/FMRI/$i/ $OUTPUT_PATH/$i/sub-${i}/ses-1/func/sub-${i}_ses-1_task-rest_bold.nii.gz
-	mv $INPUT_PATH/CoG/$i/ $OUTPUT_PATH/$i/sub-${i}/ses-1/anat/sub-${i}_ses-1_acq-highres_t1w.nii.gz
+	mkdir -p $OUTPUT_PATH/$i/sub-${i}/ses-1/func
+	mkdir -p $OUTPUT_PATH/$i/sub-${i}/ses-1/anat
+	mv $INPUT_PATH/FMRI/$i/FMRI.nii.gz $OUTPUT_PATH/$i/sub-${i}/ses-1/func/sub-${i}_ses-1_task-rest_bold.nii.gz
+    mv $INPUT_PATH/FMRI/$i/FMRI.json $OUTPUT_PATH/$i/sub-${i}/ses-1/func/sub-${i}_ses-1_task-rest_bold.json
+	mv $INPUT_PATH/CoG/$i/3D.nii.gz.gz $OUTPUT_PATH/$i/sub-${i}/ses-1/anat/sub-${i}_ses-1_acq-highres_t1w.nii.gz
+    mv $INPUT_PATH/CoG/$i/3D.json $OUTPUT_PATH/$i/sub-${i}/ses-1/anat/sub-${i}_ses-1_acq-highres_t1w.json
 done
 
