164d35a8b8679ac69358f64549b25bf796327705
spolex <spolex@msn.com>
2019-06-02 02:19:44 -0400

    estructurando los directorios de los controles nuevos

--- scripts/controls2bids.sh
+++ scripts/controls2bids.sh
@@ -11,5 +11,7 @@ array=( one two three )
 for i in "${SUBJS[@]}"
 do
 	echo $i
+	mv $INPUT_PATH/FMRI/$i/ $OUTPUT_PATH/$i/sub-${i}/ses-1/func/sub-${i}_ses-1_task-rest_bold.nii.gz
+	mv $INPUT_PATH/CoG/$i/ $OUTPUT_PATH/$i/sub-${i}/ses-1/anat/sub-${i}_ses-1_acq-highres_t1w.nii.gz
 done
 
