dbd68f297edc075957ec19e4119b3b2353023dd4
spolex <spolex@msn.com>
2019-10-07 00:36:26 -0400

    delete pplyfunction and plotter

--- preprocess_test.py
+++ preprocess_test.py
@@ -18,7 +18,6 @@ from utils import experiment_config
 # set up argparser
 parser = argparse.ArgumentParser(description="Rest fmri preprocess pipeline")
 parser.add_argument("-c","--config", type=str, help="Configuration file path, default file is config_old.json", nargs='?', default="conf/config_test.json")
-parser.add_argument("-m","--move_plot", action="store_true", help="MCFLIRT: Plot translation and rotations movement and save .par files")
 args = parser.parse_args()
 
 #load experiment configuration
@@ -98,15 +97,7 @@ TR = experiment["t_r"]
 slice_timing_correction = Node(SliceTimer(time_repetition=TR), name="slice_timer")
 
 # MCFLIRT - motion correction
-mcflirt = Node(MCFLIRT(mean_vol=True, save_plots=args.move_plot), name="mcflirt")
-
-# Plot estimated motion parametersfrom realignment
-if args.move_plot:
-    # plots
-    mc_plots = ['rotations', 'translations']
-    plotter = Node(fsl.PlotMotionParams(), name="motion_correction_plots")
-    plotter.inputs.in_source = 'fsl'
-    plotter.iterables = ('plot_type', mc_plots)
+mcflirt = Node(MCFLIRT(mean_vol=True, save_plots=True), name="mcflirt")
 
 ############################# PIPELINE #################################################################################
 # Create a preprocessing workflow
@@ -120,9 +111,6 @@ preproc.connect(selectfiles, 'anat', bet, 'in_file')
 preproc.connect(selectfiles, 'func', trim, 'in_file')
 preproc.connect(trim, 'out_file', slice_timing_correction, 'in_file')
 preproc.connect(slice_timing_correction, 'slice_time_corrected_file', mcflirt, 'in_file')
-if args.move_plot:
-    preproc.connect(mcflirt, 'par_file', plotter, 'in_file')
-
 
 preproc.connect(bet, 'out_file', datasink, 'preproc.@skull_strip')
 preproc.connect(mcflirt, 'par_file', datasink, 'preproc.@par')
