71f73a5ab1e041d78d3a940b5065ff88cfef3779
spolex <spolex@msn.com>
2019-10-07 01:16:03 -0400

    print experiment json for debug

--- preprocess_test.py
+++ preprocess_test.py
@@ -47,6 +47,7 @@ func_file = opj('{subject_id}', 'f1.nii.gz')
 templates = {'anat': anat_file,
              'func': func_file}
 
+print(experiment)
 home = experiment["files_path"]["working_dir"]
 data_dir = experiment["files_path"]["preproc"]["data_path"]
 output_dir = experiment["files_path"]["preproc"]["output"]
