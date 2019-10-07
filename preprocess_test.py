e68d1ee0eba7d7895e8928a18390caaaeed26ec8
spolex <spolex@msn.com>
2019-10-06 23:06:59 -0400

    probando nueva interfaz bet

--- preprocess_test.py
+++ preprocess_test.py
@@ -4,5 +4,5 @@ fsl.FSLCommand.set_default_output_type('NIFTI_GZ')
 
 skullstrip = fsl.BET()
 skullstrip.inputs.in_file = "/home/elekin/datos/C024/mprage.nii.gz"
-skullstrip.inputs.out_file = "home/elekin/results/C024/skull_mprage.nii.gz"
+skullstrip.inputs.out_file = "/home/elekin/results/C024/skull_mprage.nii.gz"
 res = skullstrip.run()
