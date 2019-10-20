4d1e2c5ccec53954d79fb25e7c77f5f005a52649
Iñigo <jisanchez003@ikasle.ehu.es>
2019-10-20 19:57:35 -0400

    refactor of extract cerebellum function

--- fmriapi.py
+++ fmriapi.py
@@ -13,19 +13,22 @@ def extract_cbl(func_file, confound_file, masker, rdo_dir):
     :param masker:
     :return:
     """
-    logging.debug("Extractin cerebellum from subject: "+func_file)
-    masker_timeseries_each_subject = masker.transform(func_file, confounds=confound_file)
+    logging.debug("Extracting cerebellum from subject: "+func_file)
+    ts = masker.transform(func_file, confounds=confound_file)
     #rdo_dir = '/'.join(func_file.split('/')[0:-1]) + '/cbl'
     if not op.exists(rdo_dir):
         create_dir(rdo_dir)
-    np.savetxt(rdo_dir + "/cbl_extracted_ts.csv", masker_timeseries_each_subject, delimiter=",")
+    np.savetxt(rdo_dir + "/cbl_extracted_ts.csv", ts, delimiter=",")
     fig = plt.figure()
-    plt.plot(masker_timeseries_each_subject)
+    plt.plot(ts)
     plt.xlabel('')
     plt.ylabel('')
     fig.savefig(rdo_dir + "/masker_extracted_ts" + ".png")
     plt.close()
+    logging.debug("Time series extracted from cerebellum")
     # save cbl image
     cbl_filename = rdo_dir + '/cbl_extracted.nii.gz'
-    cbl_img = masker.inverse_transform(masker_timeseries_each_subject)
+    cbl_img = masker.inverse_transform(ts)
     cbl_img.to_filename(op.join(cbl_filename))
+    logging.debug("Saved extracted image of cerebellum")
+    return ts
