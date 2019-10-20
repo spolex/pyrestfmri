c5c4127eba9e6eedda72adb540be8aaeac9698d5
Iñigo <jisanchez003@ikasle.ehu.es>
2019-10-20 18:24:21 -0400

    adjusting cerebellum extractor

--- cerebellum_extractor.py
+++ cerebellum_extractor.py
@@ -6,21 +6,21 @@ import numpy as np
 from os import path as op
 import matplotlib.pyplot as plt
 
-
-
 parser = argparse.ArgumentParser(description="Cerebellum atlas based region extractor")
 parser.add_argument("-v","--verbose", type=int, help="verbose leevel, default 10", nargs='?', default=10)
-parser.add_argument("-e", "--standarize", action="store_true", help="If standardize is True, the time-series are centered and normed: their mean is put to 0 and their variance to 1 in the time dimension.")
+parser.add_argument("-e", "--standarize", action="store_true", help="If standardize is True, the time-series are "
+                                                                    "centered and normed: their mean is put to 0 and "
+                                                                    "their variance to 1 in the time dimension.")
 parser.add_argument("-c","--config", type=str, help="Configuration file path", nargs='?', default="conf/config_old.json")
 parser.add_argument("-m",'--maps', help='Use mapsmasker instead of masker', action='store_true')
-args=parser.parse_args()
+args = parser.parse_args()
 
 # get experiment configuration
 config = experiment_config(args.config)
 experiment = config["experiment"]
 
 logging.getLogger().setLevel(experiment["log_level"])
-logging.basicConfig(filename=experiment["files_path"]["cbl_extractor"]["log"], filemode='w', format="%(asctime)s - %(levelname)s - %(message)s")
+logging.basicConfig(filename=experiment["files_path"]["r_extractor"]["log"], filemode ='w', format="%(asctime)s - %(levelname)s - %(message)s")
 
 # set up working dir
 data_dir = experiment["files_path"]["preproc_data_dir"]
@@ -29,7 +29,6 @@ data_dir = experiment["files_path"]["preproc_data_dir"]
 labels = np.array(experiment["labels"])
 subject_list = np.array(experiment["subjects_id"])
 logging.debug("Subject ids: " + str(subject_list))
-
 
 ts_image = experiment["files_path"]["ts_image"]
 cf_file = experiment["files_path"]["preproc"]["noise_components"]
@@ -47,15 +46,14 @@ confounds_components = list(flatmap(lambda session: map(lambda subj:op.join(data
 
 TR = experiment["t_r"]
 atlas_filename = experiment["files_path"]["cbl_extractor"]["cbl_atlas"]
+
 masker = NiftiMapsMasker(maps_img=atlas_filename, memory='nilearn_cache', memory_level=1, detrend=True,
                              verbose=args.verbose, t_r=TR)
-masker = NiftiMasker()
 masker.fit()
-print(func_filenames)
+print(masker.get_params())
 
 for filename, confound in zip(func_filenames, confounds_components):
-    print(filename)
-    print(confound)
+    logging.debug(filename)
     masker_timeseries_each_subject = masker.transform(filename, confounds=confound)
     rdo_dir = '/'.join(filename.split('/')[0:-1])+'/cbl'
     if not op.exists(rdo_dir):
