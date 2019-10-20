6cffdc93c752b501d5acd3f145f4f4fa4e653ed9
Iñigo <jisanchez003@ikasle.ehu.es>
2019-10-19 20:23:44 -0400

    add cerebellum extractor

--- cerebellum_extractor.py
+++ cerebellum_extractor.py
@@ -1,27 +1,16 @@
-# In[]:
 from utils import create_dir,flatmap,experiment_config, update_experiment,plot,plot_connectcome,plot_extracted
-
-#import nilearn modules
-from nilearn.connectome import ConnectivityMeasure
-
-from os import path as op
-import os
+import argparse
+from nilearn.input_data import NiftiMapsMasker
 import logging
 import numpy as np
-from nilearn import (image, plotting)
-from nilearn.regions import RegionExtractor
-from nilearn.input_data import NiftiMapsMasker
+from os import path as op
 import matplotlib.pyplot as plt
-import argparse
 
-parser = argparse.ArgumentParser(description="Functional atlas based region extractor")
-parser.add_argument("-c","--config", type=str, help="Configuration file path", nargs='?', default="conf/config_old.json")
-parser.add_argument("-i","--highpass", type=float, help="high pass filter, default none", nargs='?', default=None)
-parser.add_argument("-l","--lowpass", type=float, help="low pass filter, default none", nargs='?', default=None)
+
+
+parser = argparse.ArgumentParser(description="Cerebellum atlas based region extractor")
 parser.add_argument("-v","--verbose", type=int, help="verbose leevel, default 10", nargs='?', default=10)
 parser.add_argument("-e", "--standarize", action="store_true", help="If standardize is True, the time-series are centered and normed: their mean is put to 0 and their variance to 1 in the time dimension.")
-parser.add_argument("-s","--fwhm", type=float, help="Smooth threshold, default None", nargs='?', default=None)
-
 args=parser.parse_args()
 
 # get experiment configuration
@@ -31,27 +20,18 @@ experiment = config["experiment"]
 logging.getLogger().setLevel(experiment["log_level"])
 logging.basicConfig(filename=experiment["files_path"]["r_extractor"]["log"], filemode ='w', format="%(asctime)s - %(levelname)s - %(message)s")
 
+# set up working dir
+data_dir = experiment["files_path"]["preproc_data_dir"]
+
 # set up files' path
 labels = np.array(experiment["labels"])
 subject_list = np.array(experiment["subjects_id"])
 logging.debug("Subject ids: " + str(subject_list))
 
-# set up working dir
-data_dir = experiment["files_path"]["preproc_data_dir"]
 
-
-#In[]:read components image from file
-
-TR = experiment["t_r"]
-session_list = [1] # sessions start in 1 TODO allow more than one session
-
-# set up ts file path and name from working_dir
-ts_image = experiment["files_path"]["ts_image"]
-logging.info("Timeseries image path: "+ts_image)
+ts_image = "../../datos/TEMPLATES/Cerebellum-MNIfnirt-prob-2mm.nii.gz"
 cf_file = experiment["files_path"]["preproc"]["noise_components"]
-cbl_dir = data_dir+'/'+'cerebelo'
-create_dir(cbl_dir)
-
+session_list = [1] # sessions start in 1 TODO allow more than one session
 
 #set up data dirs
 subjects_pref = map(lambda subject: '_subject_id_'+(subject), subject_list)
@@ -63,20 +43,20 @@ input_dirs = map(lambda session: map(lambda subj:op.join(data_dir,subj),session)
 func_filenames = list(flatmap(lambda session: map(lambda subj:op.join(data_dir,subj,ts_image),session),sessions_subjects_dir))
 confounds_components = list(flatmap(lambda session: map(lambda subj:op.join(data_dir,subj,cf_file),session),sessions_subjects_dir))
 
-# Loading atlas image stored in 'maps'
-cbl_img = experiment["files_path"]["brain_atlas"]["cerebellum"]
+TR = experiment["t_r"]
+atlas_filename = "../../datos/TEMPLATES/Cerebellum-MNIfnirt-prob-2mm.nii.gz"
 
-#Region extracted
-extractor = RegionExtractor(cbl_img, verbose=args.verbose, thresholding_strategy='ratio_n_voxels',
-                            extractor='local_regions', memory="nilearn_cache", memory_level=2,
-                            t_r=TR, high_pass=args.highpass, low_pass=args.lowpass, standardize=args.standarize,
-                            min_region_size=args.region_size)
-
-extractor.fit()
-# Extracted regions are stored in regions_img_
-regions_extracted_img = extractor.regions_img_
-# Total number of regions extracted
-n_regions_extracted = regions_extracted_img.shape[-1]
-# Each region index is stored in index_
-regions_index = extractor.index_
-
+masker = NiftiMapsMasker(low_pass=args.lowpass, high_pass=args.highpass, t_r=TR,
+                           maps_img=atlas_filename, standardize=args.standarize,
+                           memory='nilearn_cache', verbose=args.verbose, smoothing_fwhm=args.fwhm)
+masker.fit()
+for filename, confound in zip(func_filenames, confounds_components):
+    masker_timeseries_each_subject = masker.transform(filename,confounds=confound)
+    filename = '/'.join(filename.split('/')[0:-1])+'/cbl'
+    np.savetxt(filename+"/cbl_extracted_ts.csv",masker_timeseries_each_subject, delimiter=",")
+    fig = plt.figure()
+    plt.plot(masker_timeseries_each_subject)
+    plt.xlabel('')
+    plt.ylabel('')
+    fig.savefig(filename+"/masker_extracted_ts" + ".png")
+    plt.close()
