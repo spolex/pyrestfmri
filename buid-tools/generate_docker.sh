d66408f241ea7b095c2d341e3fd615dd37a490dd
spolex <spolex@msn.com>
2022-08-27 01:20:38 -0400

    update build tools

--- buid-tools/generate_docker.sh
+++ buid-tools/generate_docker.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Generate Dockerfile or Singularity recipe.
-docker run --rm repronim/neurodocker:master generate docker \
+docker run --rm repronim/neurodocker:0.7.0 generate docker \
   --label maintainer="Inigo Sanchez <jisanchez003@ehu.es>" \
   --base=neurodebian:stretch-non-free \
   --pkg-manager=apt \
@@ -12,7 +12,7 @@ docker run --rm repronim/neurodocker:master generate docker \
   --ants version=2.3.1 \
   --user elekin \
   --workdir='/home/elekin'\
-  --run pip3 install nitime nipype matplotlib nibabel nitime numpy dcm2niix scikit-learn pygpu pandas nilearn \
+  --run "pip3 install nitime nipype matplotlib nibabel nitime numpy dcm2niix scikit-learn pygpu pandas nilearn" \
   --volume /home/elekin/datos \
   --volume /home/elekin/pyrestfmri \
   --volume /home/elekin/results: \
